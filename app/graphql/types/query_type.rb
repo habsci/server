a3b0a582a3489a2543114ad61f55474d655a8197
Jeffrey Goldsmith <jeff.goldsmith@shopify.com>
2018-09-17 15:26:24 -0400

    Changed rails app to api only application

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,6 +5,7 @@ module Types
 
     field :all_sensor_data, [Types::SensorDataType], null: true
     field :all_users, [Types::UserType], null: true
+    field :all_services, [Types::ServiceType], null: true
 
     def all_sensor_data
       SensorData.all
@@ -12,6 +13,10 @@ module Types
 
     def all_users
       User.all
+    end
+
+    def all_services
+      Service.all
     end
   end
 end
